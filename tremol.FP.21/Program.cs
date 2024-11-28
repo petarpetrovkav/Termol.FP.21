fb77b58d3480c7968db782a88574ca64241d6275
petar_petrov <petar.petrov@codex.mk>
2024-11-28 10:14:49 +0100

    First Commit

---
+++ tremol.FP.21/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO.Ports;
+using System.Management;
+using System.Net.Sockets;
+using System.Net;
+using System.Text.Json;
+
+
+try
+{
+    using (Tremol.FP test = new Tremol.FP())
+    {
+        test.OpenPort("COM9", 115200);
+        test.PrintLogo();
+        test.PrintText("Test 1...");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
